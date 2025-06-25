4ed812d5e37dfc9840aaf6a5486abbd23093b2e2
Nikita-1432011 <steppers.nikita3159@gmail.com>
2025-06-25 18:56:11 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,8 @@
+bday1 = input("Enter your birthday")
+bday2 = input("Enter your birthday")
+bday3 = input("Enter your birthday")
+bday4 = input("Enter your birthday")
+bday5 = input("Enter your birthday")
+
+print ("The birthday of aarohi is on",bday1,"\n The birthday of ridhima is on",bday2,"\n The birthday of saanjh is on",bday3,"\n The birthday of ishwari is on",bday4,"\n The birthday of kinjal is on",bday5)
+
